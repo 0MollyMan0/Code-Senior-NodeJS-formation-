d1f99b43e2ddc657dc84f11df6b6c2c19e36a8d9
0MollyMan0 <fougereantoine0@gmail.com>
2025-05-28 14:45:58 +0200

    added nodemon devDependencies

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Hello, Node!');
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req,res) => res.send('Hello, Express!'))
+
+app.listen(port, () =>console.log(`Notre application Node est démarée sur : http://localhost:${port}`))
